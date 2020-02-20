c41cb5a2fb636ea03807fa4dd523dfdd7e6547b7
PeacefullNoob <59912238+PeacefullNoob@users.noreply.github.com>
2020-02-20 15:06:56 +0100

    Update 3.0

--- resources/views/app/about.blade.php
+++ resources/views/app/about.blade.php
@@ -12,23 +12,94 @@
           <div class="site-mobile-menu-body"></div>
         </div>
 
-  <main class="myMain">
+  <main class="myMain" style="color:white;">
         <div class = "container-fluid">
           <div class = "row justify-content-center">
-            <div class = "col-md-5" >
-              <figure class="figure">
-              <img src="/images/aboutPhoto.jpg" id="picAbout" class="figure-img img-fluid rounded"data-aos="fade-up">
-              <figcaption class="figure-caption">A caption for the above image.</figcaption>
-              </figure>
-                <div class="row" style="padding-top:30px;" data-aos="zoom-in">
-                  <div class="col-md-12">
-                      <p style="color:white;" data-aos="fade-left">Lorem ipsum dolor sit amet, consectetur  elit. Ipsa explicabo quasi cum, laudantium neque at veniam itaque atque  temporibus! Beatae sit soluta magni neque autem, suscipit dolorem, quo alias.</p>
-                      <p style="color:white;" data-aos="fade-right">Similique deserunt sit accusamus ipsum optio. Quia, sapiente saepe culpa ad nemo velit,numquam impedit voluptate quo tempore. Perferendis suscipit dolores, ducimus esse cupiditate possimus quae quis iusto rem?</p>
-                      <p style="color:white;" data-aos="fade-left">Excepturi soluta maxime velit vitae tempore corporis, aliquid quidem modi libero! Optio minima esse hic repellendus a dolor perferendis dolore impedit quis et! Minus maxime itaque beatae totam eos reiciendis.</p>
-                      <p class="mb-5" style="color:white;" data-aos="fade-right">Ad veritatis eos dicta, animi et voluptates iusto, excepturi corporis amet, laboriosam, officiis libero. Odio excepturi aliquid suscipit nobis odit. Tenetur expedita impedit error consequatur sunt voluptatum voluptate voluptatem vitae!</p>
-                      <p cass="mt-4" style="color:white;">Thanks! <br> Lorem ipsum </p>
+            <div class = "col-md-9" >
+            <div class = "col-md-12 about" >
+<h4>
+             VIDEO PRODUCTION
+</h4>
+            <div class = "col-md-12 about" >
+            <p>  There is so much spectacular ways to see things. There are many stories that nature wants you to remember and we are here to document every single one of them. We are highly motivated individuals inspired by nature, architecture & buildings, exteriors, interiors, food and people. We tell outdoor stories in a way that is relatable to your audience. Customer's approach to every task we are working on is crucial thing for success. If you are someone who appreciate these values, we would love to work with you.
+            </p>
+          </div>
+    <div class = "col-md-12 about" >
+<h4>
+     PHOTO SESSIONS
+</h4>
+    </div>
+
+    <div class = "col-md-10 about" >
+   <p> Transfering an ambient, experience or atmosphere in just a single framerate could be very difficult. Despite that, we are always ready for the challenge. Higher challenges motivate us even more so we are always hyped up about these kind of tasks. 
+Even if you think you have a really hard one for us, bring it on because one thing is for sure - there is nohing that can surprise us.</p>
+</div>
+
+<div class = "col-md-12 about" >
+  <h4>
+ MARKETING SERVICES
+</h4>
+</div>
+
+<div class = "col-md-12 about" >
+<p>Through years of working in creative industries, we have an intimate knowledge of your audience, and an understanding of the challenges particular to outdoor and real estate marketing. Starting from the point of customer's need, we can easily recognize right direction and way of marketing communication for your business or brand. </p>
+</div>
+
+<div class = "row justify-content-center about">
+
+
+                  <div class="col-md-12 text-center about1">
+                   <h4> NORD TEAM</h4>
                   </div>
+                  <div class="col-md-10 ">
+
+                  <div class="card-deck about">
+  <div class="card" style="    background-color: transparent;">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Aleksandar</h5>
+      <p class="text-muted">Drone pilot, video editor, hiker, biker, nature lover. Love eating domestic food, can wake up at 4:30am if needed to take a great shot.</p>
+    </div>
+  </div>
+  <div class="card" style="    background-color: transparent;">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Andrej</h5>
+      <p class="text-muted">Camera man, photography guru. There is a belief that he could tell story of a lifetime in one single frame.</p>
+    </div>
+  </div>
+  <div class="card" style="    background-color: transparent;">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Nada</h5>
+      <p class="text-muted">Visual storyteller, creative director, snowboarder, crossfiter. Should spend less time injuring himself by doing sports.</p>
+    </div>
+  </div>
+</div>
+</div>
+</div>
                 </div>
+                <div class = "row justify-content-center about1">
+                  
+                <div class="col-md-6 text-center">
+                   <h4> Why you should work with us?
+</h4>
+</div>
+                  </div> 
+                  <div class = "row justify-content-center about">
+
+                  <div class="col-md-5 text-center">
+               <p><i>
+               To show outdoor and real estate, you need to know outdoor and real estate.
+We eat, sleep, breath outdoors and real estate.
+Personal, compelling and valued stories are key.
+The beauty of nature is in everything we make.
+We understand your audience, we understand your goals.
+We travel and produce light, with equipment adapted to the task at hand.
+</i>
+</p>
+                  </div>
+                  </div>
             </div>
             </div>
 

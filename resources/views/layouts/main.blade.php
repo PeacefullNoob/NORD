789fcd8a041426592292eb62f9ed65b1a5a1b0b0
PeacefullNoob <59912238+PeacefullNoob@users.noreply.github.com>
2020-02-21 14:30:23 +0100

    Update 3.1

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,22 +2,33 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+<link rel="icon" type="image/png" href="/images/Bar-ikonica.png"  />
+<link rel="apple-touch-icon-precomposed" href="/images/Bar-ikonica.png" type="image/png" sizes="152x152"/>
+<link rel="apple-touch-icon-precomposed" href="/images/Bar-ikonica.png" type="image/png" sizes="120x120"/>
+<link rel="apple-touch-icon" href="/images/Bar-ikonica.png" sizes="180x180"/>
+
+
         <link rel="stylesheet" href="/css/normalize.css"> 
         <link rel="stylesheet" href="/css/gallery.css">
         <link rel="stylesheet" href="/css/bootstrap.min.css">
         <link rel="stylesheet" href="/css/jquery-ui.css">
         <link href="https://fonts.googleapis.com/css?family=Fira+Sans&display=swap" rel="stylesheet">
-
-<!--         <link href="https://fonts.googleapis.com/css?family=Kanit:300,900&display=swap" rel="stylesheet">
- -->        <link rel="stylesheet" href="/css/aos.css">
+        <link rel="stylesheet" href="/css/aos.css">
         <link rel="stylesheet" href="/css/style.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      
+
 
 <title> Nord </title>
 </head>
   <body onload="myFunction()" >
       <div id="loader-wrapper">     
         <div id="loader"></div>
+<!--         <div id="tekstLoader">
+<h6> Made by QQRIQ</h6>
+</div> -->
         <div class="loader-section section-left"></div>
         <div class="loader-section section-right"></div> 
       </div>
